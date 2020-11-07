a850ef5a32e1c2e02096ec1930d11f089f960b72
Aurel Weinhold <aurel@weinhold.org>
2020-11-07 14:56:04 +0100

    Beginnings of a kprintf

--- driver/serial.c
+++ driver/serial.c
@@ -1,3 +1,9 @@
+#include <stdarg.h>
+
+// TODO(aurel): Move this to a util library!!
+enum bool { true = 1, false = 0 };
+typedef enum bool bool;
+
 #define SERIAL_BASE (0x7E201000 - 0x3F000000)
 
 struct serial {
@@ -28,5 +34,59 @@ static volatile struct serial* const serial_i = (struct serial*)SERIAL_BASE;
 
 void kputchar(char c)
 {
-	serial_i->dr = c;
+    // TODO(aurel): Check if buffer is empty? What else needs to happen to make this safe?
+    serial_i->dr = c;
+}
+
+void kprintf(const char* format, ...)
+{
+    va_list args;
+    va_start(args, format);
+
+    const char* cur_char = format;
+    unsigned int count = 0;
+    while (!(*cur_char)) {
+	if (*cur_char == '%') {
+	    switch (*(++cur_char)) {
+	    case 'c':
+	    case 's':
+	    case 'x':
+	    case 'i':
+	    case 'u':
+	    case 'p':
+		count++;
+		break;
+	    case '%':
+		break;
+	    default:
+		// NOTE: Error!
+		return;
+	    }
+	}
+	cur_char++;
+    }
+
+    char* string[count];
+
+    while (!(*cur_char)) {
+	if (*cur_char != '%') {
+	    kputchar(*cur_char);
+	} else {
+	    switch (*(++cur_char)) {
+	    case 'c':
+	    case 's':
+	    case 'x':
+	    case 'i':
+	    case 'u':
+	    case 'p':
+	    case '%':
+		count++;
+		break;
+	    default:
+		// NOTE: Error!
+		return;
+	    }
+	}
+	cur_char++;
+    }
 }
