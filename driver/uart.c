c1b6d80eb46e7af959ff6f3edaf403061169767c
Aurel Weinhold <aurel@weinhold.org>
2020-11-10 22:09:53 +0100

    Preparation for advanced handling of the UART.
    - Information on the bit field of the flag register (fr) of the UART

--- driver/uart.c
+++ driver/uart.c
@@ -2,13 +2,21 @@
 
 #define UART_BASE (0x7E201000 - 0x3F000000)
 
+enum fr_bit_field {
+    FR_BUSY = 3, // busy transmitting data
+    FR_RXFE = 4, // receive FIFO empty
+    FR_TXFF = 5, // transmit FIFO full
+    FR_RXFF = 6, // receive FIFO full
+    FR_TXFE = 7	 // transmit FIFO empty
+};
+
 struct uart {
 	unsigned int dr; // data register
 	unsigned int rsrecr;
 	unsigned int padding0[4];
 	unsigned int fr; // flag register
 	unsigned int padding1;
-	unsigned int ilpr;
+	unsigned int ilpr; // unused
 	unsigned int ibrd;
 	unsigned int fbrd;
 	unsigned int lcrh;
@@ -31,12 +39,15 @@ static volatile struct uart* const uart_i = (struct uart*)UART_BASE;
 void kputchar(unsigned char c)
 {
 	// TODO(aurel): Check if buffer is empty? What else needs to happen to make this safe?
+	// Check flag register (FR) TXFF (bit 5) - Transmit FIFO full
+	// Maybe also BUSY (bit 3) - busy transmitting data
 	uart_i->dr = c;
 }
 
 unsigned char kgetchar()
 {
 	// TODO(aurel): Check if buffer has been written to? What else needs to happen to make this safe?
+	// Check flag register (FR) RXFE (bit 4) - Receive FIFO empty
 	char c = (unsigned char)(uart_i->dr & 0xff);
 	return c;
 }
