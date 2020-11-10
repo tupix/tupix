03d582c73ee4d5e1e37ed19ae816f787e7a22cbd
druckdev <druckdev@protonmail.com>
2020-11-10 16:59:43 +0100

    Formating again

--- driver/led.c
+++ driver/led.c
@@ -3,26 +3,25 @@
 #define GPF_BITS 3
 
 enum gpio_func {
-	gpio_input = 0x0, 	// GPIO Pin is an input
-	gpio_output = 0x1, 	// GPIO Pin is an output
+	gpio_input = 0x0,  // GPIO Pin is an input
+	gpio_output = 0x1, // GPIO Pin is an output
 };
 
 struct gpio {
-    unsigned int func[6];
-    unsigned int unused0;
-    unsigned int set[2];
-    unsigned int unused1;
-    unsigned int clr[2];
+	unsigned int func[6];
+	unsigned int unused0;
+	unsigned int set[2];
+	unsigned int unused1;
+	unsigned int clr[2];
 };
 
-static volatile
-struct gpio * const gpio_port = (struct gpio *)GPIO_BASE;
+static volatile struct gpio* const gpio_port = (struct gpio*)GPIO_BASE;
 
 void yellow_on(void)
 {
-    /* Initialisieren */
-    gpio_port->func[0] = gpio_output << (YELLOW_LED * GPF_BITS);
+	/* Initialisieren */
+	gpio_port->func[0] = gpio_output << (YELLOW_LED * GPF_BITS);
 
-    /* Anschalten */
-    gpio_port->set[0] = 1 << YELLOW_LED;
+	/* Anschalten */
+	gpio_port->set[0] = 1 << YELLOW_LED;
 }
