d266fcf7b58478d615927016f6985e185d4cccc9
Aurel Weinhold <aurel@weinhold.org>
2020-11-12 16:33:16 +0100

    Transmit FIFO must not be empty

--- driver/uart.c
+++ driver/uart.c
@@ -1,13 +1,15 @@
 #include <driver/uart.h>
 
+#include <std/util.h>
+
 #define UART_BASE (0x7E201000 - 0x3F000000)
 
 enum fr_bit_field {
-	FR_BUSY = 3, // busy transmitting data
-	FR_RXFE = 4, // receive FIFO empty
-	FR_TXFF = 5, // transmit FIFO full
-	FR_RXFF = 6, // receive FIFO full
-	FR_TXFE = 7	 // transmit FIFO empty
+	FR_BUSY = 0x3, // busy transmitting data
+	FR_RXFE = 0x4, // receive FIFO empty
+	FR_TXFF = 0x5, // transmit FIFO full
+	FR_RXFF = 0x6, // receive FIFO full
+	FR_TXFE = 0x7  // transmit FIFO empty
 };
 
 struct uart {
@@ -47,7 +49,13 @@ void kputchar(unsigned char c)
 unsigned char kgetchar()
 {
 	// TODO(aurel): Check if buffer has been written to? What else needs to happen to make this safe?
-	// Check flag register (FR) RXFE (bit 4) - Receive FIFO empty
+
+	while (is_set(uart_i->fr, FR_RXFE)) {
+	} // wait until recieve FIFO is not empty
 	char c = (unsigned char)(uart_i->dr & 0xff);
+	// TODO(aurel): Maybe set FR->RXFE to 1 to signify that its empty again?
+
+	set_bit(&(uart_i->fr), FR_RXFE);
+
 	return c;
 }
