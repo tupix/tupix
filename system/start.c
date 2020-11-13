d408a5236abfef5ca011d787774f3f7ab3b3e3df
Aurel Weinhold <aurel@weinhold.org>
2020-11-13 11:37:52 +0100

    Clean up

--- system/start.c
+++ system/start.c
@@ -8,21 +8,6 @@ void start_kernel(void)
 
 	test_kprintf();
 
-	// yellow_on();
-	// for (int i = 0; i < 10; i++) {
-	// 	for (int i = 0; i < 10; i++) {
-	// 		kputchar('a' + i);
-	// 	}
-	// 	kputchar('\n');
-	// }
-
-	//kprintf("foo %c%c%c %s %%\n", 98, 'a', 'r', "baz");
-	//kprintf("%x %i %u\n", 31, -31, 31);
-	//kprintf("%010p\n", &start_kernel);
-
-	unsigned int i = 1;
-	kprintf("%016p\n", &i);
-
 	while (1) {
 		char c = kgetchar();
 		kprintf("%c %03i %03u %x %p\n", c, c, c, c, c);
