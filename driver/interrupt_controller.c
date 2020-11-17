1d7097537b9c341d9ebecba0c197dc47e57d2168
druckdev <druckdev@protonmail.com>
2020-11-17 21:20:22 +0100

    Add irq_basic_pending enum

--- driver/interrupt_controller.c
+++ driver/interrupt_controller.c
@@ -15,6 +15,30 @@ struct interrupt_register {
 	uint32 disable_basic_irqs;
 };
 
+// The abbreviation IRQP stands for interrupt request (IRQ) pending
+enum irq_basic_pending {
+	GPU_IRQ_62 = 20,
+	GPU_IRQ_57 = 19,
+	GPU_IRQ_56 = 18,
+	GPU_IRQ_55 = 17,
+	GPU_IRQ_54 = 16,
+	GPU_IRQ_53 = 15,
+	GPU_IRQ_19 = 14,
+	GPU_IRQ_18 = 13,
+	GPU_IRQ_10 = 12,
+	GPU_IRQ_9  = 11,
+	GPU_IRQ_7  = 10,
+	IRQ_PEND_GT_0_2 = 8, // One or more bits set in pending register 2
+	IRQ_PEND_GT_0_1 = 8, // One or more bits set in pending register 1
+	ILL_ACCESS_TYPE_0_IRQP = 7, // Illegal access type 0 IRQ pending
+	ILL_ACCESS_TYPE_1_IRQP = 6, // Illegal access type 1 IRQ pending
+	GPU1_HALTED_IRQP = 5, // GPU1 halted IRQ pending
+	GPU0_HALTED_IRQP = 4, // GPU0 halted IRQ pending
+	ARM_DOORBELL_1_IRQP = 3, // ARM Doorbell 1 IRQ pending
+	ARM_DOORBELL_0_IRQP = 2, // ARM Doorbell 0 IRQ pending
+	ARM_MAILBOX_IRQP = 1, // ARM Mailbox IRQ pending
+	ARM_TIMER_IRQP = 0, // ARM Timer IRQ pending
+};
 // Bit field for irq_pending, enable_irqs and disable_irqs
 enum irq_table {
 	uart_int = 57,
