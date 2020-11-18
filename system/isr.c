f9be036b69f8ad8c4e5d050458218629e6e93753
Aurel Weinhold <aurel@weinhold.org>
2020-11-18 16:09:33 +0100

    Simple debug prints to find out if interrupts work

--- system/isr.c
+++ system/isr.c
@@ -5,34 +5,43 @@
  *       but at least the code compiles now
  */
 
+#include <std/io.h>
 void reset()
 {
+    kprintf("reset");
 }
 
 void undefined_instruction()
 {
+    kprintf("undefined instruction");
 }
 
 void software_interrupt()
 {
+    kprintf("software interrupt");
 }
 
 void prefetch_abort()
 {
+    kprintf("prefetch abort");
 }
 
 void data_abort()
 {
+    kprintf("data abort");
 }
 
 void not_used()
 {
+    kprintf("not used");
 }
 
 void irq()
 {
+    kprintf("IRQ");
 }
 
 void fiq()
 {
+    kprintf("FIQ");
 }
