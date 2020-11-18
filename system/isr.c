6a8aeae185078c06bb74571c7a23c4968e31de97
Aurel Weinhold <aurel@weinhold.org>
2020-11-18 13:51:53 +0100

    WIP compile-able interrupt handler functions

--- system/isr.c
+++ system/isr.c
@@ -1,5 +1,38 @@
 /* ISR - Interrupt Service Routine - Interrupt handler */
 
+/*
+ * TODO: All of these might need to be assembler functions, I don't know yet,
+ *       but at least the code compiles now
+ */
+
+void reset()
+{
+}
+
 void undefined_instruction()
+{
+}
+
+void software_interrupt()
+{
+}
+
+void prefetch_abort()
+{
+}
+
+void data_abort()
+{
+}
+
+void not_used()
+{
+}
+
+void irq()
+{
+}
+
+void fiq()
 {
 }
