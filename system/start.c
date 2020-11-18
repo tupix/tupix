e48ebaebd2dd31d18006bf92eeb4b4c9f777e0db
Aurel Weinhold <aurel@weinhold.org>
2020-11-18 16:09:07 +0100

    Call init_interrupt_controller when starting up

--- system/start.c
+++ system/start.c
@@ -1,13 +1,17 @@
 #include <config.h>
+#include <driver/interrupt_controller.h>
 #include <driver/uart.h>
 #include <std/io.h>
 
 void start_kernel(void)
 {
+	init_interrupt_controller();
 	init_uart();
 
+#if 0
 	while (1) {
 		char c = uart_getchar();
 		kprintf("%c %03i %03u %x\n", c, c, c, c);
 	}
+#endif
 }
