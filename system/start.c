0cb82b8c5135e79f374af83c5cb078b1ea0d8c46
druckdev <druckdev@protonmail.com>
2020-11-20 17:05:29 +0100

    Add main loop construct for current task

--- system/start.c
+++ system/start.c
@@ -8,11 +8,23 @@ void start_kernel(void)
 	init_interrupt_controller();
 	init_uart();
 
-	while (1) {};
-#if 0
 	while (1) {
 		char c = uart_getchar();
 		kprintf("%c %03i %03u %x\n", c, c, c, c);
+
+		switch (c) {
+		case 's':
+			// trigger Supervisor Call
+			break;
+		case 'u':
+			// trigger Undefined Instruction
+			break;
+		case 'p':
+			// trigger Prefetch Abort
+			break;
+		case 'd':
+			// trigger Data Abort
+			break;
+		}
 	}
-#endif
 }
