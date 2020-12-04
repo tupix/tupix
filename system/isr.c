d600e186b1280dec8696520018d0a0663a1a2617
Aurel Weinhold <aurel@weinhold.org>
2020-12-04 23:12:36 +0100

    SWI prepared for later.

--- system/isr.c
+++ system/isr.c
@@ -195,6 +195,8 @@ void software_interrupt_handler(void* sp)
 {
 	volatile struct registers* reg = (struct registers*)sp;
 	print_registers(reg, "Software Interrupt", "Continuing.", "");
+	// TODO(Aurel): In the future, this should continue.
+	while (true) {}
 }
 
 void prefetch_abort_handler(void* sp)
