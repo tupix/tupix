b6546ff0d393c022102417e9fb0bb779b2306264
Aurel Weinhold <aurel@weinhold.org>
2020-12-04 13:36:06 +0100

    Fix long long is 64 bit, not long.
    - apparently int and long have the same size. Go figure.

--- include/std/types.h
+++ include/std/types.h
@@ -4,9 +4,9 @@
 typedef enum bool { true = 1, false = 0 } bool;
 
 typedef int int32;
-typedef long int64;
+typedef long long int64;
 
 typedef unsigned int uint32;
-typedef unsigned long uint64;
+typedef unsigned long long uint64;
 
 #endif /* STD_TYPES_H */
