9dbbb7c9a1fed3c82aada10f781c0388caefe0ae
Aurel Weinhold <aurel@weinhold.org>
2020-12-04 13:56:44 +0100

    Initialize UART before timer
    - also cleanup

--- system/start.c
+++ system/start.c
@@ -7,32 +7,6 @@
 void start_kernel(void)
 {
 	init_interrupt_controller();
-	init_local_timer();
 	init_uart();
-
-#if 0
-	while (1) {
-		char c = uart_getchar();
-		kprintf("%c %03i %03u %x\n", c, c, c, c);
-
-		switch (c) {
-		case 's':
-			// trigger Supervisor Call
-			asm("svc #0");
-			break;
-		case 'u':
-			// trigger Undefined Instruction
-			asm("udf");
-			break;
-		case 'p':
-			// trigger Prefetch Abort
-			asm("bkpt");
-			break;
-		case 'd':
-			// trigger Data Abort
-			// TODO: trigger
-			break;
-		}
-	}
-#endif
+	init_local_timer();
 }
