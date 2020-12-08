e4713e2506f2bb40e11ef0415436864feeccc6c4
druckdev <druckdev@protonmail.com>
2020-12-08 11:56:12 +0100

    Add memcpy

---
+++ include/std/mem.h
@@ -0,0 +1,11 @@
+#ifndef STD_MEM_H
+#define STD_MEM_H
+
+#include <data/types.h>
+
+#define NULL 0x0
+
+void* memcpy(const void* src, void* dest, uint32 n);
+// TODO: memmove()
+
+#endif /* STD_MEM_H */
---
+++ lib/std/mem.c
@@ -0,0 +1,17 @@
+#include <std/mem.h>
+
+#include <data/types.h>
+
+/*
+ * Copies `n` bytes from `src` to `dest`. The memory areas must not overlap.
+ * Returns: `dest`
+ */
+void* memcpy(const void* src, void* dest, uint32 n)
+{
+	char* c_src	 = (char*)src;
+	char* c_dest = (char*)dest;
+	for (uint32 i = 0; i < n; ++i)
+		c_dest[i] = c_src[i];
+
+	return dest;
+}
