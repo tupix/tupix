a356baf40d11602d573972121be8c13b1fcafe15
Aurel Weinhold <aurel@weinhold.org>
2020-12-11 18:08:51 +0100

    Very simple Round Robin scheduler
    Implementation of a very simple round robin scheduler based on the
    simple threads and a simple ringbuffer and fifo implementation.
    Threads should be created and added to the waiting queue before starting
    the scheduler. See Note in include/system/scheduler.h.

--- include/system/scheduler.h
+++ include/system/scheduler.h
@@ -2,7 +2,25 @@
 #define SYSTEM_SCHEDULER_H
 
 #include <data/types.h>
+#include <system/thread.h>
 
-void thread_create(void (*func)(void*), const void* args, uint32 args_size);
+/*
+ * NOTE(Aurel): Simple scheduler with super simple threads.
+ * To create a thread call `create_thread`. It will create a super simple thread
+ * with a unique id and a callback to a simple function which prints the threads
+ * id for a short while.
+ * To start the scheduler initialize it first by calling `init_scheduler` and
+ * then start it. It will dequeue the threads, run them and then queue them
+ * again causing an (purposeful) infinite loop.
+ *
+ * NOTE(Aurel): Because of the infinite loop and because the scheduler is not
+ * yet based on interrupts, you need to create threads before starting the
+ * scheduler otherwise it will just dequeue the empty thread and return.
+ */
+
+void init_scheduler();
+
+void schedule_thread(struct tcb);
+void start_scheduler();
 
 #endif /* SYSTEM_SCHEDULER_H */
--- system/scheduler.c
+++ system/scheduler.c
@@ -3,15 +3,70 @@
 #include <data/types.h>
 #include <system/thread.h>
 
-struct queue_tcb {
-	struct tcb tcb;
-	struct queue_tcb* prev;
-	struct queue_tcb* next;
+#include <std/io.h>
+#include <std/log.h>
+#include <std/mem.h>
+
+struct thread_q {
+	uint32 size;
+	uint32 tail, head;
+	struct tcb threads[N_THREADS];
 };
 
-struct queue_tcb thread_blocks[N_THREADS];
+static volatile struct thread_q waiting_q;
 
 void
-thread_create(void (*func)(void*), const void* args, uint32 args_size)
+init_scheduler()
 {
+	waiting_q.size = N_THREADS;
+}
+
+// NOTE(Aurel): Do not increment var when using this macro.
+#define circle_forward(var, size) (var) = (var) + 1 >= (size) ? 0 : (var) + 1
+
+void
+queue(struct tcb thread)
+{
+	waiting_q.threads[waiting_q.head] = thread;
+	log(LOG, "Queued thread %i.", thread.id);
+	circle_forward(waiting_q.head, waiting_q.size);
+	if (waiting_q.head == waiting_q.tail) {
+		circle_forward(waiting_q.tail, waiting_q.size);
+	}
+}
+
+struct tcb
+dequeue()
+{
+	struct tcb thread = {};
+	if (waiting_q.head == waiting_q.tail) {
+		log(LOG, "Dequeued empty thread. Waiting queue empty.");
+		return thread;
+	}
+
+	thread = waiting_q.threads[waiting_q.tail];
+	circle_forward(waiting_q.tail, waiting_q.size);
+	log(LOG, "Dequeued thread %i.", thread.id);
+	return thread;
+}
+
+void
+schedule_thread(struct tcb thread)
+{
+	queue(thread);
+}
+
+void
+start_scheduler()
+{
+	while (true) {
+		struct tcb cur_thread = dequeue();
+		if (cur_thread.id == 0) {
+			// TODO(Aurel): Run default thread.
+			break;
+		} else {
+			(*(cur_thread.callback))(&cur_thread);
+			queue(cur_thread);
+		}
+	}
 }
