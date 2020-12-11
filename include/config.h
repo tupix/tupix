2d8d888cfda07d556e820edea46c2d0f9ee67956
druckdev <druckdev@protonmail.com>
2020-12-11 11:30:37 +0100

    Move LOGGING_ENABLED out of #ifdef __QEMU__ block
    Also:
    Format the block. We indented the #define's inside of the #if for better
    differentiation. We know about the IndentPPDirectives clang-format
    option but since it indents all #defines in headers we left it disabled.

--- include/config.h
+++ include/config.h
@@ -6,14 +6,14 @@
 // Timer Interrupt Interval zum testen in Mikrosekunden
 #define LOCAL_TIMER_US 1000000
 
+// clang-format off
+// QEMU is a lot faster than the actual hardware, thus needing a higher value.
 #ifdef __QEMU__
-/* Werte zum testen unter QEMU */
-#define BUSY_WAIT_COUNTER 3000000
-
+	#define BUSY_WAIT_COUNTER 3000000
 #else
-/* Werte zum testen auf der Hardware */
-#define BUSY_WAIT_COUNTER 30000
+	#define BUSY_WAIT_COUNTER 30000
+#endif /* __QEMU__ */
+// clang-format on
 
 #define LOGGING_ENABLED
 
-#endif
