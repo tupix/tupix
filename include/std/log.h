618227332c6c28de9137dd17c60ade3fbb032f4f
Aurel Weinhold <aurel@weinhold.org>
2020-12-11 10:38:31 +0100

    Add logging case ASSERT
    - Reordering of the prints.

--- include/std/log.h
+++ include/std/log.h
@@ -5,24 +5,28 @@ enum log_lvl {
 	ERROR,
 	WARNING,
 	LOG,
+	ASSERT,
 };
 
 // [Reference](https://stackoverflow.com/questions/1644868/define-macro-for-debug-printing-in-c)
 #define log(log_lvl, fmt, ...)                                                 \
 	do {                                                                       \
 		if (DEBUG_ENABLED) {                                                   \
-			kprintf("%s:%d:%s() ", __FILE__, __LINE__, __func__);              \
 			switch (log_lvl) {                                                 \
+			case ASSERT:                                                       \
+				kprintf("[ASSERT] ");                                          \
+				break;                                                         \
 			case ERROR:                                                        \
-				kprintf("[ERROR]: ");                                          \
+				kprintf("[ERROR] ");                                           \
 				break;                                                         \
 			case WARNING:                                                      \
-				kprintf("[WARNING]: ");                                        \
+				kprintf("[WARNING] ");                                         \
 				break;                                                         \
 			case LOG:                                                          \
 			default:                                                           \
-				kprintf("[LOG]: ");                                            \
+				kprintf("[LOG] ");                                             \
 			}                                                                  \
+			kprintf("%s:%d:%s(): ", __FILE__, __LINE__, __func__);             \
 			kprintf(fmt, __VA_ARGS__);                                         \
 			kprintf("\n");                                                     \
 		}                                                                      \
