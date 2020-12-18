94816bd878fd6b29d1bf3b1aeec42337c3512599
druckdev <druckdev@protonmail.com>
2020-12-18 15:31:24 +0100

    Create local context for initialized thread
    We only want to access `scheduled_thread` as soon as it exists. The
    local context helps that this happens.

--- system/thread.c
+++ system/thread.c
@@ -40,15 +40,18 @@ user_thread(void* x)
 void
 thread_create(void (*func)(void*), const void* args, size_t args_size)
 {
-	struct tcb thread = { 0 };
-	thread.callback   = func;
-	thread.regs.pc    = (uint32)func;
-	thread.regs.lr    = (uint32)&endless_loop;
-	// thread.cpsr = spsr;
-	thread.initialized = false;
-	// TODO: What else is there to be done?
+	struct tcb* scheduled_thread;
+	{
+		struct tcb thread = { 0 };
+		thread.callback   = func;
+		thread.regs.pc    = (uint32)func;
+		thread.regs.lr    = (uint32)&endless_loop;
+		// thread.cpsr = spsr;
+		thread.initialized = false;
+		// TODO: What else is there to be done?
 
-	struct tcb* scheduled_thread = schedule_thread(&thread);
+		scheduled_thread = schedule_thread(&thread);
+	}
 	if (!scheduled_thread)
 		return; // Thread was not added to queue
 
