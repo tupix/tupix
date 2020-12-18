81cf6b0dc84f07a1dc3dcb45ec1a9f7bf624128e
druckdev <druckdev@protonmail.com>
2020-12-18 14:18:54 +0100

    Move user_thread into own src and header file

--- system/thread.c
+++ system/thread.c
@@ -19,25 +19,6 @@ get_stack_pointer(const size_t index)
 	return stack;
 }
 
-// TODO: Own header and source file?
-#define PRINT_N 10
-void
-user_thread(void* x)
-{
-	size_t id = *(size_t*)x;
-	x += sizeof(id);
-	uint32 c = *(uint32*)x;
-	x += sizeof(c);
-	for (size_t i = 0; i < PRINT_N; ++i) {
-		log(DEBUG, "thread %i: %c", id, c);
-		kprintf("%c", c);
-		// We need a volatile counter so that the loop is not optimized out.
-		for (volatile uint32 i = 0; i < BUSY_WAIT_COUNTER * 10; ++i) {}
-	}
-	log(DEBUG, "thread %i: done", id);
-	return;
-}
-
 void
 exit_thread()
 {
