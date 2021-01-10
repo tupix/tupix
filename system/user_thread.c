c8bf5bb63f0586c9b088c58a1857f173cc6141d3
druckdev <druckdev@protonmail.com>
2021-01-11 00:35:48 +0100

    Readd assignment specific exception throwing
    This reverts commit 81a42c05e7505317ab1e3edc85d4e26837756c17.

--- system/user_thread.c
+++ system/user_thread.c
@@ -14,6 +14,25 @@ user_thread(void* x)
 {
 	uint32 c = *(uint32*)x;
 	x += sizeof(c);
+	switch (c) {
+	case 's':
+		trigger_exception(SUPERVISOR_CALL);
+		break;
+	case 'p':
+		trigger_exception(PREFETCH_ABORT);
+		break;
+	case 'a':
+		trigger_exception(DATA_ABORT);
+		break;
+	case 'u':
+		trigger_exception(UNDEFINED_INSTRUCTION);
+		break;
+	case 'c':
+		register_checker();
+		break;
+	default:
+		break;
+	}
 	for (size_t i = 0; i < PRINT_N; ++i) {
 		kprintf("%c", c);
 		// We need a volatile counter so that the loop is not optimized out.
