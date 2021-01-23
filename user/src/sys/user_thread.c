a749de57e3b870a1832ba6dbff05d6644011a592
druckdev <druckdev@protonmail.com>
2021-01-23 15:02:15 +0100

    Add reasonable user_thread

--- user/src/sys/user_thread.c
+++ user/src/sys/user_thread.c
@@ -3,45 +3,17 @@
 void
 user_thread(void* args)
 {
-	if (!args)
-		while (1)
-			putchar('f');
-}
-
-#if 0
-#define PRINT_N 10
-void
-user_thread(void* x)
-{
-	uint32 c = 'f';
-	if (x) {
-		c = *(uint32*)x;
-		x += sizeof(c);
-		switch (c) {
-		case 's':
-			trigger_exception(SUPERVISOR_CALL);
-			break;
-		case 'p':
-			trigger_exception(PREFETCH_ABORT);
-			break;
-		case 'a':
-			trigger_exception(DATA_ABORT);
-			break;
-		case 'u':
-			trigger_exception(UNDEFINED_INSTRUCTION);
-			break;
-		case 'c':
-			register_checker();
-			break;
-		default:
-			break;
-		}
+	if (args) {
+		uint32 c = *(uint32*)args;
+		args += sizeof(c);
+		putchar(c);
 	}
-	for (size_t i = 0; i < PRINT_N; ++i) {
-		kprintf("%c", c);
-		// We need a volatile counter so that the loop is not optimized out.
-		for (volatile uint32 i = 0; i < BUSY_WAIT_COUNTER * 10; ++i) {}
-	}
-	return;
+	putchar('f');
+	wait(0);
+	uint32 c;
+	c = getchar();
+	putchar(c);
+	create_thread(&user_thread, &c, sizeof(c));
+	exit();
+	putchar('x');
 }
-#endif
