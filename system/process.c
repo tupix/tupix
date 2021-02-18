528e1f6efa1af83a3fecb6b3866d8cdbce9679f8
Aurel Weinhold <aurel@weinhold.org>
2021-02-18 21:29:34 +0100

    On process creation copy udata into memory
    Copy the entire udata section, already in kernel memory, into the new
    processes memory. This way all the processes have their respective copy
    of the udata segment, shared between all their threads.

--- system/process.c
+++ system/process.c
@@ -1,7 +1,14 @@
 #include <data/types.h>
 
+#include <driver/mmu.h>
+
 #include <system/process.h>
 #include <system/scheduler.h>
+
+#include <std/mem.h>
+
+extern char _udata_begin[], _udata_end[], _udata_cpy_begin[];
+#define UDATA_SIZE _udata_end - _udata_begin
 
 /*
  * TODO(Aurel): Index queues to place the threads in the processes list. This
@@ -23,6 +30,15 @@ process_create(void (*func)(void*), const void* args, size_t args_size)
 	}
 
 	init_process_memory(process->l2_table);
+
+	// copy in the user data
+	switch_memory(process->pid, process->l2_table);
+
+	memcpy(_udata_begin, _udata_cpy_begin, UDATA_SIZE);
+
+	// TODO(Aurel): Cleanup into own function?
+	struct pcb* cur_process = get_cur_process();
+	switch_memory(cur_process->pid, cur_process->l2_table);
 
 	thread_create(process, func, args, args_size);
 	klog(LOG, "Done creating new process. (p%u)", process->pid);
