4786d8b0c7f906b8ca296cad2e14743bf376007b
Aurel Weinhold <aurel@weinhold.org>
2021-02-18 21:38:06 +0100

    Add test program for separated udata
    Activate by setting TEST_UDATA_SEPERATION to 1 in
    `user/src/sys/user_thread`.

--- user/src/sys/user_thread.c
+++ user/src/sys/user_thread.c
@@ -2,13 +2,36 @@
 #include <std/io.h>
 #include <sys/calls.h>
 
-extern uint32 _kstacks_start[];
+#define TEST_UDATA_SEPERATION 0
+
+# if TEST_UDATA_SEPERATION
+char PRINT_CHAR;
+unsigned int GLOB_COUNTER = 100;
+#endif
 
 void
 endless_loop()
 {
 	while (1) {}
 }
+
+# if TEST_UDATA_SEPERATION
+void
+countdown(void* args)
+{
+	uint32 i = 0;
+	if (args) {
+		i = *(uint32*)args;
+		args += sizeof(i);
+	}
+
+	while (GLOB_COUNTER > 0) {
+		printf("%c,%i: %i\n", PRINT_CHAR, i, GLOB_COUNTER);
+		GLOB_COUNTER--;
+		wait(2);
+	}
+}
+#endif /* TEST_UDATA_SEPERATION */
 
 void
 user_thread(void* args)
@@ -19,8 +42,16 @@ user_thread(void* args)
 		args += sizeof(c);
 	}
 
+#if TEST_UDATA_SEPERATION
+	PRINT_CHAR = c;
+	uint32 one = 1, two = 2, three = 3;
+	create_thread(&countdown, &one, sizeof(c));
+	create_thread(&countdown, &two, sizeof(c));
+	create_thread(&countdown, &three, sizeof(c));
+#else /* TEST_UDATA_SEPERATION */
 	printf("Hello World! ");
 	printf("%c\n", c);
+#endif /* TEST_UDATA_SEPERATION */
 }
 
 void
