69bfe242d5155608fdb9529db74a8726f6bdabdf
druckdev <druckdev@protonmail.com>
2021-04-19 13:42:08 +0200

    Explicitly use the .clang-format

--- format.sh
+++ format.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-find . \( -name "*.c" -o -name "*.h" \) -a -exec clang-format-11 -i '{}' ';'
+find . \( -name "*.c" -o -name "*.h" \) -a -exec clang-format-11 --style=file -i '{}' ';'
