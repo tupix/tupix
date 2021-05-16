199ec07d4cb7f6c1b341872f0160fa6a75a6bac2
Aurel Weinhold <aurel@weinhold.org>
2021-05-16 19:29:37 +0200

    Move and slightly adjust printf description

--- user/include/std/io.h
+++ user/include/std/io.h
@@ -1,6 +1,9 @@
 #ifndef STD_IO_H
 #define STD_IO_H
 
+/*
+ * Print output according to formatstring `format`.
+ */
 void printf(const char* format, ...) __attribute__((format(printf, 1, 2)));
 
 #endif /* STD_IO_H */
